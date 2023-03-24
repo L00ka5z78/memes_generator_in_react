ac95eab0e57ca7f13dd932ba2d081864f5803819
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-03-24 07:36:14 +0100

    small fixes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import React from "react";
 import { Header } from "./MemeGenerator/Header";
 import { Meme } from "./MemeGenerator/Meme";
 
 
 export default function App() {
-
-
   return (
     <div>
       <Header />
@@ -16,5 +13,3 @@ export default function App() {
     </div>
   );
 }
-
-
